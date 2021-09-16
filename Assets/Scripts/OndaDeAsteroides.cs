b20a9dc4801eebe9b39629563908a2924d835ca1
abmoraes16 <a.bmoraes@yahoo.com>
2021-09-16 20:41:45 -0300

    Fix: asteroid fragments

--- Assets/Scripts/OndaDeAsteroides.cs
+++ Assets/Scripts/OndaDeAsteroides.cs
@@ -6,22 +6,21 @@ public class OndaDeAsteroides : MonoBehaviour
 {
     public ComportamentoAsteroide comportamentoAsteroide;
     public int quantidadeAsteroides = 3;
-    // Start is called before the first frame update
+
     void Start()
+    {
+        GerarAsteroides();
+    }
+
+    public void GerarAsteroides()
     {
         for(int i=0; i < quantidadeAsteroides; i++)
         {
             float x = Random.Range(-7.0f, 7.0f);
             float y = Random.Range(-4.0f, 4.0f);
+
             Vector3 posicao = new Vector3(x, y, 0.0f);
             Instantiate(comportamentoAsteroide, posicao, Quaternion.identity);
         }
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
